cd6e529a74aff78f8626b551190a534261d2fbab
naveenjohnsonv <14140949+naveenjohnsonv@users.noreply.github.com>
2021-11-10 22:18:18 +0550

    raphael: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: 1 Manufacturer: xiaomi Model: Redmi K20 Pro Script version: 2.1.0 Signed-off-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_raphael.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_raphael-user \
+    omni_raphael-userdebug \
+    omni_raphael-eng
